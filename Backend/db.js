1b6f78da08248af5c6769937d5231782876e16ef
sanketkalola <sanketkalola878@gmail.com>
2025-06-30 15:22:21 +0530

    Your commit message

--- Backend/db.js
+++ Backend/db.js
@@ -1,13 +1,15 @@
-const mongoose = require('mongoose')
-const mongoURI = "mongodb://127.0.0.1:27017/IMS";
+const mongoose = require('mongoose');
+
+const mongoURI = process.env.MONGO_URI;
 
 const connectToMongo = async () => {
   try {
     mongoose.set("strictQuery", false);
-    mongoose.connect(mongoURI);
+    await mongoose.connect(mongoURI);
     console.log("Connected to Mongo Successfully!");
   } catch (error) {
-    console.log(error);
+    console.error("Mongo connection error:", error);
   }
 };
+
 module.exports = connectToMongo;
