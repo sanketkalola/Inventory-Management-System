e3aba113099836d5d62acfc7409e78d4fd689e9f
sanketkalola <sanketkalola878@gmail.com>
2025-07-01 18:45:26 +0530

    Add live demo link

--- Backend/Models/Products.js
+++ Backend/Models/Products.js
@@ -1,19 +1,26 @@
 const mongoose = require('mongoose');
-const ProductSchema = new mongoose.Schema(
-    {
-        ProductName: {
-            type: String,
-            required: true,
-        },
-        ProductPrice: {
-            type: Number,
-            required: true,
-        },
-        ProductBarcode: {
-            type: Number,
-            required: true,
-        },
-    });
 
-const Products = mongoose.model("Products", ProductSchema)
+const ProductSchema = new mongoose.Schema({
+  ProductName: {
+    type: String,
+    required: true,
+  },
+  ProductPrice: {
+    type: Number,
+    required: true,
+  },
+  ProductBarcode: {
+    type: Number,
+    required: true,
+    validate: {
+      validator: function (v) {
+        // Convert number to string and check length and digits
+        return /^\d{12}$/.test(v.toString());
+      },
+      message: props => `${props.value} is not a valid 12-digit barcode!`,
+    },
+  },
+});
+
+const Products = mongoose.model("Products", ProductSchema);
 module.exports = Products;
